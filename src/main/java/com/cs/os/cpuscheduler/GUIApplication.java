f10e9f179681c7152b07550a743b107e9bd5e7fc
Zakaria-019R <zakaria.elmaachi19@gmail.com>
2024-04-29 23:35:16 +0200

    Added the scrollpanes

--- src/main/java/com/cs/os/cpuscheduler/GUIApplication.java
+++ src/main/java/com/cs/os/cpuscheduler/GUIApplication.java
@@ -11,7 +11,7 @@ public class GUIApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GUIApplication.class.getResource("CPUScheduler.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 560);
         scene.getStylesheets().add(getClass().getResource("app.css").toExternalForm());
         stage.setTitle("CPU Scheduler Simulator");
         stage.setScene(scene);
